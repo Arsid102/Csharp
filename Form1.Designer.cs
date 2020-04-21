fdd75bc6f6ae819eab25f0b6646cda720ce9d7b4
arsidcurri <arsid.curri@leerling.go-atheneumoudenaarde.be>
2020-04-21 09:40:12 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace flappiebird {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pnlgame = new System.Windows.Forms.Panel();
+            this.tmrgame = new System.Windows.Forms.Timer(this.components);
+            this.tmrachtergrond = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(734, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "NEW GAME";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pnlgame
+            // 
+            this.pnlgame.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pnlgame.Location = new System.Drawing.Point(12, 81);
+            this.pnlgame.Name = "pnlgame";
+            this.pnlgame.Size = new System.Drawing.Size(734, 528);
+            this.pnlgame.TabIndex = 1;
+            this.pnlgame.Click += new System.EventHandler(this.stijgen);
+            this.pnlgame.Paint += new System.Windows.Forms.PaintEventHandler(this.allesWeergeven);
+            // 
+            // tmrgame
+            // 
+            this.tmrgame.Tick += new System.EventHandler(this.Tmrgame_Tick);
+            // 
+            // tmrachtergrond
+            // 
+            this.tmrachtergrond.Tick += new System.EventHandler(this.Tmrachtergrond_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 621);
+            this.Controls.Add(this.pnlgame);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "flappy bird";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlgame;
+        private System.Windows.Forms.Timer tmrgame;
+        private System.Windows.Forms.Timer tmrachtergrond;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace flappiebird {
+    public partial class Form1 : Form {
+        bool botsing;
+        int score;
+        int afstandHorizontaal, afstandVerticaal;
+        string gamestatus;
+        int ybird;
+        int pastijd;
+        Bitmap achtergrond, imgbuisboven, imgbuisonder, imgbird;
+        Random rnd;
+        Graphics g;
+        Rectangle rbird;
+        Rectangle[] buisboven, buisonder;
+
+        private void Tmrachtergrond_Tick(object sender, EventArgs e) {
+            toonbuizen();
+            pastijd++;
+            changebg();
+            Console.WriteLine("pasde tijd: "+pastijd);
+            pnlgame.Invalidate();
+        }
+
+        private void changebg()
+        {
+            if (pastijd <= 50)
+            {
+                achtergrond = new Bitmap("media/bg.jpg");
+                pnlgame.BackgroundImageLayout = ImageLayout.Stretch;
+            }            
+            else if (pastijd <= 100)
+            {
+                achtergrond = new Bitmap("media/bg2.jpg");
+                pnlgame.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            else if (pastijd <= 150)
+            {
+                achtergrond = new Bitmap("media/bg3.jpg");
+                pnlgame.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+            if (pastijd >= 200) pastijd = 0;
+        }
+
+        private void toonbuizen() {
+            ybird += 5;
+            if (buisboven[0].X <=0-imgbuisonder.Width-afstandHorizontaal) {
+                score += 2;
+                maakbuizen(pnlgame.Width - rnd.Next(50, 120));
+            } else {
+                for (int i = 0; i < buisboven.Length; i++) {
+                    buisboven[i].X -= 20;
+                    buisonder[i].X -= 20;
+                }
+            }
+        }
+
+        private void Tmrgame_Tick(object sender, EventArgs e) {
+            for (int i = 0; i < buisonder.Length; i++) {
+                botsing = buisonder[i].IntersectsWith(rbird) || buisboven[i].IntersectsWith(rbird) || (ybird + 40) > pnlgame.Height;
+                if (botsing) break;
+            }
+            if (botsing) {
+                tmrgame.Stop();
+                tmrachtergrond.Stop();
+                gamestatus = "Game Over";
+            } else {
+                ybird += rnd.Next(-2, 6);
+                pnlgame.Invalidate();
+            }
+        }
+
+        private void stijgen(object sender, EventArgs e) {
+            ybird -= 35;
+            pnlgame.Invalidate();
+        }
+
+        private void allesWeergeven(object sender, PaintEventArgs e) {
+            g = e.Graphics;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.DrawImage(achtergrond, new Rectangle(0, 0, 6500, 5000), 0, 0, pnlgame.Width, pnlgame.Height, GraphicsUnit.Millimeter, null);
+            for (int i = 0; i < buisonder.Length; i++) {
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(imgbuisboven, buisboven[i], 0, 0, imgbuisboven.Width, imgbuisboven.Height, GraphicsUnit.Pixel, null);
+                g.DrawRectangle(new Pen(Color.Red), buisboven[i]);
+                g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                g.DrawImage(imgbuisonder, buisonder[i], 0, 0, imgbuisonder.Width, imgbuisonder.Height, GraphicsUnit.Pixel, null);
+                g.DrawRectangle(new Pen(Color.Red), buisonder[i]);
+            }
+            //vogel
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            rbird = new Rectangle(80, ybird, 60, 40);
+            g.DrawImage(imgbird, rbird, -5, -3, 60, 60, GraphicsUnit.Millimeter, null);
+            g.DrawRectangle(new Pen(Color.Red), rbird);
+            Font fn = new Font("Callibri", 24, FontStyle.Bold);
+            g.DrawString(gamestatus, fn, new SolidBrush(Color.Yellow), rbird.X + 70, rbird.Y);
+            g.DrawString(score.ToString(), fn, new SolidBrush(Color.Yellow), 30, 50);
+        }
+
+        public Form1() {
+            InitializeComponent();
+            gameitemsconfig();
+        }
+        private void Button1_Click(object sender, EventArgs e) {
+            pastijd = 0;
+            gameitemsconfig();
+            tmrachtergrond.Start();
+            tmrgame.Start();
+        }
+        private void gameitemsconfig() {
+            typeof(Panel).InvokeMember("DoubleBuffered", BindingFlags.SetProperty|BindingFlags.Instance | BindingFlags.NonPublic, null, pnlgame, new object[] {true});
+            botsing = false;
+            rnd = new Random();
+            score = 0;
+            gamestatus = "";
+            afstandHorizontaal = 330;
+            afstandVerticaal = 110;
+            changebg();
+            imgbuisboven = new Bitmap("media/buisonder.png");
+            imgbuisboven.MakeTransparent();
+            imgbuisonder = new Bitmap("media/buisboven.png");
+            imgbuisonder.MakeTransparent();
+            buisboven = new Rectangle[2];
+            buisonder = new Rectangle[2];
+            maakbuizen(600);
+            imgbird = new Bitmap("media/bird4.png");
+            imgbird.MakeTransparent();
+            ybird = pnlgame.Height / 2;
+        }
+
+        private void maakbuizen(int beginx) {
+            int[] hoogtenieuwpaar = new int[] { 0, rnd.Next(20, 70) };
+            for (int i = 0; i < buisboven.Length; i++) {
+                int ybegin = -60 + hoogtenieuwpaar[i];
+                //rectangle (pos x, pos y, breedte, hoogte)
+                buisonder[i] = new Rectangle(beginx + (afstandHorizontaal * i), ybegin, imgbuisonder.Width, imgbuisonder.Height);
+                buisboven[i] = new Rectangle(beginx + (afstandHorizontaal * i), ybegin+ afstandVerticaal + imgbuisonder.Height, imgbuisboven.Width, imgbuisboven.Height);
+            }
+            Console.WriteLine(buisonder[0].Y + ' ' + buisonder[1].Y);
+        }
+    }
+}
